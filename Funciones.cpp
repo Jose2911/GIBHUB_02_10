f910c0c668d14259fecb331a64b48ca67bf0a1fc
Jose2011 <jose.vidal@utec.edu.pe>
2019-10-04 15:24:07 -0500

    Así se puede añadir más archivos

---
+++ Funciones.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Usuario on 4/10/2019.
+//
+
+
---
+++ Funciones.h
@@ -0,0 +1,8 @@
+//
+// Created by Usuario on 4/10/2019.
+//
+
+#ifndef GIBHUB_02_10_FUNCIONES_H
+#define GIBHUB_02_10_FUNCIONES_H
+
+#endif //GIBHUB_02_10_FUNCIONES_H
